4b965a0b96a7eef356c1ee70d2f8d27e737a1b03
nimurr <nimurnerob404@gmail.com>
2025-12-31 14:35:32 +0600

    `Updated PopularMoviesSection.js and movie/[id]/page.js with minor styling and layout changes`

--- src/app/(main)/movie/[id]/page.js
+++ src/app/(main)/movie/[id]/page.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 const Page = () => {
     return (
-        <div className='min-h-screen pt-40 text-center text-5xl capitalize text-white'>
-            detail Page
+        <div className="max-w-7xl mx-auto px-4 md:px-0 py-10 md:py-20 grid grid-cols-4 gap-5">
+            <div className='col-span-1 bg-blue-500 p-5'>
+                sidebar
+            </div>
+            <div className='col-span-3 bg-red-500 p-5'>
+                main
+            </div>
         </div>
     );
 }
