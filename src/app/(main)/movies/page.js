f049770b1555681095033eab0bf738cf4e124bae
nimurr <nimurnerob404@gmail.com>
2026-01-06 18:05:24 +0600

    `Added modal functionality to movies page`

--- src/app/(main)/movies/page.js
+++ src/app/(main)/movies/page.js
@@ -1,16 +1,50 @@
+'use client';
 import AllMoviewMainContent from '@/Components/movies/AllMoviewMainContent';
 import AllMoviewSidebar from '@/Components/movies/AllMoviewSidebar';
-import React from 'react';
+import React, { useState } from 'react';
+import { IoClose } from "react-icons/io5";
 
 const Page = () => {
+    const [modalOpen, setModalOpen] = useState(true);
+    const [modalImage, setModalImage] = useState('');
+
+    // Function to open modal with an image
+    const openModal = (imageUrl) => {
+        setModalImage(imageUrl);
+        setModalOpen(false);
+    };
+
     return (
         <div className="max-w-7xl mx-auto px-4 md:px-0 py-10 md:py-20 grid grid-cols-4 gap-5">
+            {/* Sidebar */}
             <div className='col-span-1 w-full'>
                 <AllMoviewSidebar />
             </div>
+
+            {/* Main content */}
             <div className='col-span-3'>
-                <AllMoviewMainContent />
+                {/* Pass openModal to your main content if you want clicking images to open modal */}
+                <AllMoviewMainContent openModal={openModal} />
             </div>
+
+            {/* Modal */}
+            {modalOpen && (
+                <div className="fixed inset-0 bg-black/70 flex items-center justify-center z-50">
+                    <div className="relative bg-transparent p-5 rounded-xl max-w-lg w-full">
+                        <button
+                            onClick={() => setModalOpen(false)}
+                            className="absolute top-2 right-2 text-white text-2xl hover:text-gray-300"
+                        >
+                            <IoClose />
+                        </button>
+                        <img
+                            src={'https://i.ibb.co.com/8nSPBNBD/adventure-movie-poster-template-design-7b13ea2ab6f64c1ec9e1bb473f345547-screen.jpg'}
+                            alt="Modal"
+                            className="w-full h-auto rounded-lg"
+                        />
+                    </div>
+                </div>
+            )}
         </div>
     );
 }
