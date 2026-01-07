fde18575e5ec7767582c411172af8e40a2a68a87
nimurr <nimurnerob404@gmail.com>
2026-01-07 16:35:24 +0600

    `Update community page layout and add title`

--- src/app/(main)/community/page.js
+++ src/app/(main)/community/page.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Page = () => {
     return (
-        <div className='h-screen py-20 text-white'>
-           
+        <div className='h-screen py-24 text-white'>
+            <h2 className='text-3xl font-semibold text-center'>Community</h2>
         </div>
     );
 }
